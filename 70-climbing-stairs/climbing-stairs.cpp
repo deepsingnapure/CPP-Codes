11cfa9151bb9f9072694dc5695579840e8daab6d
Deep Singnapure <deepsingnapure5@gmail.com>
2026-03-20 19:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (33.26%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp;
+    int helper(int n){
+        if(n == 1) return 1;
+        if(n == 2)return 2;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = helper(n-1)+helper(n-2);
+    }
+    int climbStairs(int n) {
+        dp.resize(n+1,-1);
+        return helper(n);
+    }
+};
